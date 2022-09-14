765312f5351eb6180092caea45837d3672b1c5e9
nabilaateya <natheya36@gmail.com>
2022-09-14 08:27:02 +0700

    Initial commit

---
+++ app/src/main/java/com/xhateya/idn/myintentapp/MoveActivity.kt
@@ -0,0 +1,13 @@
+package com.xhateya.idn.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+
+
+    }
+}
---
+++ app/src/main/java/com/xhateya/idn/myintentapp/Person.kt
@@ -0,0 +1,13 @@
+package com.xhateya.idn.myintentapp
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Person(
+    val name : String?,
+    val age : Int?,
+    val email : String?,
+    val city : String?
+): Parcelable
+
